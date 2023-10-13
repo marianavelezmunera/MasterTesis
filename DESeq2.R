5b4207ab0493bd4e0e2fdd59ff5ef8e73a6b7be0
Mariana <marianavelez.35@gmail.com>
2023-10-13 11:48:03 -0600

    commit

--- DESeq2.R
+++ DESeq2.R
@@ -93,7 +93,7 @@ ggplot(sig_table_rizo, aes(x=Family, y=log2FoldChange, color=Class)) + geom_poin
 
 ggsave("DESeq_rizo.png",last_plot())
 
-## Heatmap
+## Heatmap general
 
 sig_table1<-sig_table[order(sig_table$padj,na.last = NA),]
 sig_table1<-sig_table1[1:20,]
@@ -129,3 +129,72 @@ pheatmap(matrix, scale= "row",
          cellwidth = 10,
          filename = "DESeq2_heatmap.png")
 
+
+
+## Heatmap rizosfera
+
+sig_table_rizo1<-sig_table_rizo[order(sig_table_rizo$padj,na.last = NA),]
+sig_table_rizo1<-sig_table_rizo1[1:20,]
+taxa_sig_rizo<-rownames(sig_table_rizo1)
+ps.taxa.rel_rizo <- transform_sample_counts(hongos_rizosfera, function(x) x/sum(x)*100)
+ps.taxa.rel.sig_rizo <- prune_taxa(taxa_sig_rizo, ps.taxa.rel_rizo)
+matrix_rizo <- as.matrix(data.frame(otu_table(ps.taxa.rel.sig_rizo)))
+rownames(matrix_rizo) <- as.character(tax_table(ps.taxa.rel.sig_rizo)[, "Species"])
+metadata_sub_rizo <- data.frame(sample_data(ps.taxa.rel.sig_rizo))
+
+annotation_col_rizo = data.frame(
+  Elevation = as.factor(metadata_sub_rizo$Altitud), 
+  check.names = FALSE
+)
+rownames(annotation_col_rizo) <- rownames(metadata_sub_rizo)
+annotation_row_rizo = data.frame(
+  Phylum = as.factor(tax_table(ps.taxa.rel.sig_rizo)[, "Phylum"])
+)
+phylum_col_rizo = RColorBrewer::brewer.pal(length(levels(annotation_row_rizo$Phylum)), "Paired")
+names(phylum_col_rizo) = levels(annotation_row_rizo$Phylum)
+ann_colors_rizo = list(
+  Elevation = c("1978"=colores[1],"2007"=colores[2],"2018"=colores[3],"2178"=colores[4],"2210"=colores[5])
+)
+
+colores<-moma.colors("Flash",5)
+colores2<-moma.colors("Picabia",3)
+pheatmap(matrix_rizo, scale= "row", 
+         annotation_col = annotation_col_rizo,
+         annotation_colors = ann_colors_rizo,
+         color = moma.colors("Kippenberger",type="continuous"),
+         cellwidth = 10,
+         filename = "DESeq2_heatmap_rizo.png")
+
+## Heatmap filosfera
+
+sig_table_filo1<-sig_table_filo[order(sig_table_filo$padj,na.last = NA),]
+sig_table_filo1<-sig_table_filo[1:20,]
+taxa_sig_filo<-rownames(sig_table_filo)
+ps.taxa.rel_filo <- transform_sample_counts(hongos_filosfera, function(x) x/sum(x)*100)
+ps.taxa.rel.sig_filo <- prune_taxa(taxa_sig_filo, ps.taxa.rel_filo)
+matrix_filo <- as.matrix(data.frame(otu_table(ps.taxa.rel.sig_filo)))
+rownames(matrix_filo) <- as.character(tax_table(ps.taxa.rel.sig_filo)[, "Species"])
+metadata_sub_filo <- data.frame(sample_data(ps.taxa.rel.sig_filo))
+
+annotation_col_filo = data.frame(
+  Elevation = as.factor(metadata_sub_filo$Altitud), 
+  check.names = FALSE
+)
+rownames(annotation_col_filo) <- rownames(metadata_sub_filo)
+annotation_row_filo = data.frame(
+  Phylum = as.factor(tax_table(ps.taxa.rel.sig_filo)[, "Phylum"])
+)
+phylum_col_filo = RColorBrewer::brewer.pal(length(levels(annotation_row_filo$Phylum)), "Paired")
+names(phylum_col_filo) = levels(annotation_row_filo$Phylum)
+ann_colors_filo = list(
+  Elevation = c("1978"=colores[1],"2007"=colores[2],"2018"=colores[3],"2178"=colores[4],"2210"=colores[5])
+)
+
+colores<-moma.colors("Flash",5)
+colores2<-moma.colors("Picabia",3)
+pheatmap(matrix_filo, scale= "row", 
+         annotation_col = annotation_col_filo,
+         annotation_colors = ann_colors_filo,
+         color = moma.colors("Kippenberger",type="continuous"),
+         cellwidth = 10,
+         filename = "DESeq2_heatmap_filo.png")
